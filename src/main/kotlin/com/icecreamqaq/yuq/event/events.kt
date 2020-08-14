51e74cc29975d12c433191056a9b98fcb371fc0d
IceCream <www@withdata.net>
2020-08-14 20:47:47 +0800

    添加发送消息的事件

--- src/main/kotlin/com/icecreamqaq/yuq/event/events.kt
+++ src/main/kotlin/com/icecreamqaq/yuq/event/events.kt
@@ -9,6 +9,7 @@ import com.icecreamqaq.yuq.entity.Friend
 import com.icecreamqaq.yuq.entity.Group
 import com.icecreamqaq.yuq.entity.Member
 import com.icecreamqaq.yuq.message.Message
+import com.icecreamqaq.yuq.message.MessageSource
 
 open class MessageEvent(open val sender: Contact, val message: Message) : Event(), CancelEvent {
     override fun cancelAble() = true
@@ -31,8 +32,9 @@ open class BotLevelGroupEvent(val group: Group) : GroupListEvent()
 
 open class NewRequestEvent(val message: String) : Event(), CancelEvent {
     override fun cancelAble() = true
-    var accept:Boolean? = null
+    var accept: Boolean? = null
 }
+
 open class NewFriendRequestEvent(val qq: Long, message: String) : NewRequestEvent(message)
 open class GroupInviteEvent(val group: Long, val qq: Long, message: String) : NewRequestEvent(message)
 open class GroupMemberRequestEvent(val group: Group, val qq: Long, val name: String, message: String) : NewRequestEvent(message), CancelEvent {
@@ -40,7 +42,7 @@ open class GroupMemberRequestEvent(val group: Group, val qq: Long, val name: Str
     val blackList = false
 }
 
-open class GroupMemberEvent(val group: Group,val member: Member) : Event()
+open class GroupMemberEvent(val group: Group, val member: Member) : Event()
 open class GroupMemberJoinEvent(group: Group, member: Member) : GroupMemberEvent(group, member)
 open class GroupMemberInviteEvent(group: Group, member: Member, val inviter: Member) : GroupMemberJoinEvent(group, member)
 open class GroupMemberLeaveEvent(group: Group, member: Member) : GroupMemberEvent(group, member)
@@ -52,11 +54,19 @@ open class GroupBanBotEvent(group: Group, member: Member, val operator: Member, 
 open class GroupUnBanBotEvent(group: Group, member: Member, val operator: Member) : GroupMemberEvent(group, member)
 
 open class ContextSessionCreateEvent(session: ContextSession) : Event()
-open class ActionContextInvokeEvent(val context: BotActionContext) : Event() , CancelEvent {
+open class ActionContextInvokeEvent(val context: BotActionContext) : Event(), CancelEvent {
     override fun cancelAble() = true
     open class Per(context: BotActionContext) : ActionContextInvokeEvent(context) {
 
     }
-    open class Post(context: BotActionContext) : ActionContextInvokeEvent(context){
+
+    open class Post(context: BotActionContext) : ActionContextInvokeEvent(context) {
     }
+}
+
+open class SendMessageEvent(val sendTo: Contact, val message: Message){
+    open class Per(sendTo: Contact, message: Message): SendMessageEvent(sendTo, message),CancelEvent{
+        override fun cancelAble() = true
+    }
+    open class Post(sendTo: Contact, message: Message, messageSource: MessageSource): SendMessageEvent(sendTo, message)
 }
