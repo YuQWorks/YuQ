a0afca301f1b356f9a98e0f71ee3901ace2ab59f
IceCream <www@withdata.net>
2020-08-14 20:31:02 +0800

    添加默认的 Controller 以避免路由为 null

---
+++ src/main/kotlin/com/icecreamqaq/yuq/controller/z.kt
@@ -0,0 +1,10 @@
+package com.icecreamqaq.yuq.controller
+
+import com.icecreamqaq.yuq.annotation.GroupController
+import com.icecreamqaq.yuq.annotation.PrivateController
+
+@PrivateController
+class ZPC
+
+@GroupController
+class ZGC
