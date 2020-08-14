13bc97ba60de59af0e4d141dc9f7bb53a9a6574c
IceCream <www@withdata.net>
2020-08-14 20:31:26 +0800

    添加默认的 MessageFactory 实现

--- src/main/kotlin/com/icecreamqaq/yuq/message/MessageFactory.kt
+++ src/main/kotlin/com/icecreamqaq/yuq/message/MessageFactory.kt
@@ -12,3 +12,29 @@ interface MessageFactory {
     fun newTemp(group:Long,qq:Long):Message
 
 }
+
+class MessageFactoryImpl : MessageFactory {
+    override fun newMessage(): Message {
+        return Message()
+    }
+
+    override fun newGroup(group: Long): Message {
+        val message = newMessage()
+        message.group = group
+        return message
+    }
+
+    override fun newPrivate(qq: Long): Message {
+        val message = Message()
+        message.qq = qq
+        return message
+    }
+
+    override fun newTemp(group: Long, qq: Long): Message {
+        val message = Message()
+        message.temp = true
+        message.qq = qq
+        message.group = group
+        return message
+    }
+}
