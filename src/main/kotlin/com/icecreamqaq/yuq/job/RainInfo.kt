f791a79d405c084b40012b723307e31aae5d3fab
IceCream <www@withdata.net>
2020-09-02 03:55:12 +0800

    添加信息收集的job

---
+++ src/main/kotlin/com/icecreamqaq/yuq/job/RainInfo.kt
@@ -0,0 +1,30 @@
+package com.icecreamqaq.yuq.job
+
+import com.IceCreamQAQ.Yu.annotation.Cron
+import com.IceCreamQAQ.Yu.annotation.JobCenter
+import com.IceCreamQAQ.Yu.util.Web
+import com.icecreamqaq.yuq.RainVersion
+import com.icecreamqaq.yuq.yuq
+import javax.inject.Inject
+
+@JobCenter
+class RainInfo {
+    @Inject
+    private lateinit var version: RainVersion
+
+    @Inject
+    private lateinit var web: Web
+
+    @Cron("10m")
+    fun upInfo() {
+        web.postJSON(
+                "http://127.0.0.1:5460/YuQ/runInfo",
+                mapOf(
+                        "uid" to yuq.botId,
+                        "yv" to version.apiVersion(),
+                        "rt" to version.runtimeName(),
+                        "rv" to version.runtimeVersion()
+                )
+        )
+    }
+}
