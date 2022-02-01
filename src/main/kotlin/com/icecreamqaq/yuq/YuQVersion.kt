8a3d853b45c440b92febcad98bc5e8c4a68f873b
IceCream <www@withdata.net>
2022-02-01 13:52:32 +0800

    0.1.0.0-DEV24

--- src/main/kotlin/com/icecreamqaq/yuq/YuQVersion.kt
+++ src/main/kotlin/com/icecreamqaq/yuq/YuQVersion.kt
@@ -5,7 +5,7 @@ import com.IceCreamQAQ.Yu.annotation.AutoBind
 @AutoBind
 interface YuQVersion {
 
-    fun apiVersion() = "0.1.0.0-DEV23"
+    fun apiVersion() = "0.1.0.0-DEV24"
     fun runtimeName():String
     fun runtimeVersion():String
 
