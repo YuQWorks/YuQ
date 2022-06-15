d077530442478606cf5c5366e91aa4f4943282b7
IceCream <www@withdata.net>
2022-06-15 22:06:53 +0800

    0.1.0.0-DEV33+MultiBot-DEV2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 val baseVersion = "0.1.0.0"
 val channel = "DEV"
-val buildNum = 1
+val buildNum = 2
 
 group = "com.IceCreamQAQ"
 version = "$baseVersion-${channel}33+MultiBot-DEV$buildNum"
