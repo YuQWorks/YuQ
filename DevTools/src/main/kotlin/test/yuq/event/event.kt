f24dfa2357fc295dd87f9b638c15e90409383421
IceCream <www@withdata.net>
2023-04-06 02:27:06 +0800

    删除整个 DevTools 文件，等待框架重构完成后重写 DevTools

--- DevTools/src/main/kotlin/test/yuq/event/event.kt
+++
@@ -1,7 +0,0 @@
-package test.yuq.event
-
-import com.IceCreamQAQ.Yu.event.events.Event
-import com.icecreamqaq.yuq.FriendList
-import com.icecreamqaq.yuq.GroupList
-
-class RegisterContactEvent(val friends: FriendList, val groups: GroupList) : Event()
