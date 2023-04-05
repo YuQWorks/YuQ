5ef4584363d9e59afb45c0f0c515ea8e1926082e
IceCream <www@withdata.net>
2023-04-06 02:26:30 +0800

    移除 PathVar 实现

--- src/main/kotlin/com/icecreamqaq/yuq/message/items.kt
+++ src/main/kotlin/com/icecreamqaq/yuq/message/items.kt
@@ -1,6 +1,6 @@
 package com.icecreamqaq.yuq.message
 
-import com.icecreamqaq.yuq.contact.Contact
+import com.icecreamqaq.yuq.contact.GroupMember
 import com.icecreamqaq.yuq.mif
 
 
@@ -25,41 +25,19 @@ abstract class MessageItemBase : MessageItem, SendAble {
         return javaClass.hashCode()
     }
 
-    override fun toString() = toPath()
+    override fun toString() = logString
 }
 
 interface MessageItem : MessagePlus, SendAble {
-    fun toLocal(contact: Contact): Any
-    fun toPath(): String
-    fun convertByPathVar(type: PathVar.Type): Any?
+
+    val logString: String
 
     fun toItemChain(): MessageItemChain
-
-    fun toLogString(): String = toPath()
-
     fun equal(other: MessageItem): Boolean
 }
 
 interface Text : MessageItem {
     val text: String
-
-    override fun toPath() = text
-    override fun convertByPathVar(type: PathVar.Type): Any? = when (type) {
-        PathVar.Type.Source -> this
-        PathVar.Type.String -> text
-        PathVar.Type.Switch -> {
-            val textLow = text.toLowerCase()
-            textLow.contains("true") || text.contains("开") || text.contains("启")
-                    || textLow.contains("open") || textLow.contains("enable")
-                    || textLow.contains("on") || text.contains("是")
-        }
-        PathVar.Type.Integer -> text.toInt()
-        PathVar.Type.Long -> text.toLong()
-        PathVar.Type.Double -> text.toDouble()
-        else -> null
-    }
-
-    override fun toLogString() = "\"" + text.replace("\n", "\\n") + "\""
 
     override fun equal(other: MessageItem): Boolean {
         if (other !is Text) return false
@@ -74,14 +52,6 @@ interface Text : MessageItem {
 interface At : MessageItem {
     val user: Long
 
-    override fun toPath() = "At_$user"
-    override fun convertByPathVar(type: PathVar.Type) = when (type) {
-        PathVar.Type.String -> user.toString()
-        PathVar.Type.Long -> user
-        PathVar.Type.Double -> user.toDouble()
-        else -> null
-    }
-
     override fun equal(other: MessageItem): Boolean {
         if (other !is At) return false
         return user == other.user
@@ -89,24 +59,16 @@ interface At : MessageItem {
 }
 
 interface AtByMember : At {
-    val member: Member
+    val member: GroupMember
     override val user: Long
         get() = member.id
 
-    override fun toLogString() = "@${member.name}($user)"
+    override val logString: String
+        get() = "@${member.nameCardOrName()}($user)"
 }
 
 interface Face : MessageItem {
     val faceId: Int
-
-    override fun toPath() = "表情_$faceId"
-    override fun convertByPathVar(type: PathVar.Type) = when (type) {
-        PathVar.Type.String -> "表情_$faceId"
-        PathVar.Type.Integer -> faceId
-        PathVar.Type.Long -> faceId.toLong()
-        PathVar.Type.Double -> faceId.toDouble()
-        else -> null
-    }
 
     override fun equal(other: MessageItem): Boolean {
         if (other !is Face) return false
@@ -117,16 +79,6 @@ interface Face : MessageItem {
 interface Image : MessageItem {
     val id: String
     val url: String
-
-    override fun toPath(): String {
-        return "img_$id"
-    }
-
-    override fun convertByPathVar(type: PathVar.Type): Any? = when (type) {
-        PathVar.Type.String -> "图片"
-        PathVar.Type.Source -> this
-        else -> null
-    }
 
     override fun equal(other: MessageItem): Boolean {
         if (other !is Image) return false
@@ -145,26 +97,12 @@ interface FlashImage : Image {
         get() = image.id
     override val url: String
         get() = image.url
-
-
-    override fun convertByPathVar(type: PathVar.Type): Any? = when (type) {
-        PathVar.Type.String -> "闪照"
-        PathVar.Type.Source -> this
-        else -> null
-    }
 }
 
 interface XmlEx : MessageItem {
     val serviceId: Int
     val value: String
 
-    override fun convertByPathVar(type: PathVar.Type): Any? = when (type) {
-        PathVar.Type.String -> value
-        PathVar.Type.Source -> this
-        else -> null
-    }
-
-    override fun toPath() = "XmlMsg"
     override fun equal(other: MessageItem): Boolean {
         if (other !is XmlEx) return false
         return value == other.value && serviceId == other.serviceId
@@ -173,14 +111,6 @@ interface XmlEx : MessageItem {
 
 interface JsonEx : MessageItem {
     val value: String
-
-    override fun convertByPathVar(type: PathVar.Type): Any? = when (type) {
-        PathVar.Type.String -> value
-        PathVar.Type.Source -> this
-        else -> null
-    }
-
-    override fun toPath() = "JsonMsg"
 
     override fun equal(other: MessageItem): Boolean {
         if (other !is JsonEx) return false
@@ -192,16 +122,6 @@ interface Voice : MessageItem {
     val id: String
     val url: String
 
-    override fun toPath(): String {
-        return "Voice_$id"
-    }
-
-    override fun convertByPathVar(type: PathVar.Type): Any? = when (type) {
-        PathVar.Type.String -> "语音"
-        PathVar.Type.Source -> this
-        else -> null
-    }
-
     override fun equal(other: MessageItem): Boolean {
         if (other !is Voice) return false
         return id == other.id
@@ -210,9 +130,6 @@ interface Voice : MessageItem {
 
 interface NoImplItem : MessageItem {
     val source: Any
-
-    override fun toPath() = "NoImpl"
-    override fun convertByPathVar(type: PathVar.Type) = "NotImpl"
 
     override fun equal(other: MessageItem): Boolean {
         if (other !is NoImplItem) return false
