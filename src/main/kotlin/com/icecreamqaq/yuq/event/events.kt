0a117f57f398deb49d07b9c5390ba139f90cedf9
IceCream <www@withdata.net>
2023-04-06 02:10:50 +0800

    改动 Contact 结构

--- src/main/kotlin/com/icecreamqaq/yuq/event/events.kt
+++ src/main/kotlin/com/icecreamqaq/yuq/event/events.kt
@@ -1,4 +1,4 @@
-package com.icecreamqaq.yuq.event
+package com.icecreamqaq.bot.event
 
 import com.IceCreamQAQ.Yu.event.events.CancelEvent
 import com.IceCreamQAQ.Yu.event.events.Event
@@ -16,10 +16,10 @@ interface BotEvent {
 open class MessageEvent(open val sender: Contact, val message: Message) : Event(), CancelEvent, BotEvent {
     override fun cancelAble() = true
     override val bot: Bot
-        get() = sender.yuq
+        get() = sender.bot
 }
 
-open class GroupMessageEvent(override val sender: Member, val group: Group, message: Message) :
+open class GroupMessageEvent(override val sender: GroupMember, val group: Group, message: Message) :
     MessageEvent(sender, message)
 
 open class GuildMessageEvent(
@@ -31,14 +31,14 @@ open class GuildMessageEvent(
 
 open class PrivateMessageEvent(sender: Contact, message: Message) : MessageEvent(sender, message) {
     open class FriendMessage(override val sender: Friend, message: Message) : PrivateMessageEvent(sender, message)
-    open class TempMessage(override val sender: Member, message: Message) : PrivateMessageEvent(sender, message)
+    open class TempMessage(override val sender: GroupMember, message: Message) : PrivateMessageEvent(sender, message)
 }
 
 open class MessageRecallEvent(
     open val sender: Contact, open val operator: Contact, val messageId: Int
 ) : Event(), BotEvent {
     override val bot: Bot
-        get() = sender.yuq
+        get() = sender.bot
 }
 
 open class PrivateRecallEvent(sender: Contact, operator: Contact, messageId: Int) :
@@ -46,37 +46,37 @@ open class PrivateRecallEvent(sender: Contact, operator: Contact, messageId: Int
 
 open class GroupRecallEvent(
     val group: Group,
-    override val sender: Member,
-    override val operator: Member,
+    override val sender: GroupMember,
+    override val operator: GroupMember,
     messageId: Int
 ) : MessageRecallEvent(sender, operator, messageId)
 
 open class FriendListEvent(override val bot: Bot) : Event(), BotEvent
-open class FriendAddEvent(val friend: Friend) : FriendListEvent(friend.yuq)
-open class FriendDeleteEvent(val friend: Friend) : FriendListEvent(friend.yuq)
+open class FriendAddEvent(val friend: Friend) : FriendListEvent(friend.bot)
+open class FriendDeleteEvent(val friend: Friend) : FriendListEvent(friend.bot)
 
 open class GroupListEvent(override val bot: Bot) : Event(), BotEvent
-open class BotJoinGroupEvent(val group: Group) : GroupListEvent(group.yuq)
+open class BotJoinGroupEvent(val group: Group) : GroupListEvent(group.bot)
 
 /***
- * com.icecreamqaq.yuq.Bot 从某个群离开。
+ * com.icecreamqaq.bot.Bot 从某个群离开。
  * 当事件响应前，group 就已经从列表中被移出。
  */
-open class BotLeaveGroupEvent(val group: Group) : GroupListEvent(group.yuq) {
+open class BotLeaveGroupEvent(val group: Group) : GroupListEvent(group.bot) {
     /***
-     * com.icecreamqaq.yuq.Bot 主动退出某群。
+     * com.icecreamqaq.bot.Bot 主动退出某群。
      */
     open class Leave(group: Group) : BotLeaveGroupEvent(group)
 
     /***
-     * com.icecreamqaq.yuq.Bot 因为某些特殊原因离开某群（其他客户端主动退出，群解散，群被强制解散等等）
+     * com.icecreamqaq.bot.Bot 因为某些特殊原因离开某群（其他客户端主动退出，群解散，群被强制解散等等）
      */
     open class Other(group: Group) : BotLeaveGroupEvent(group)
 
     /***
-     * com.icecreamqaq.yuq.Bot 被某群移出。
+     * com.icecreamqaq.bot.Bot 被某群移出。
      */
-    open class Kick(val operator: Member) : BotLeaveGroupEvent(operator.group)
+    open class Kick(val operator: GroupMember) : BotLeaveGroupEvent(operator.group)
 }
 
 open class NewRequestEvent(override val bot: Bot, val message: String) : Event(), CancelEvent, BotEvent {
@@ -97,44 +97,44 @@ open class GroupInviteEvent(bot: Bot, val group: GroupInfo, val qq: UserInfo, me
 
 open class GroupMemberRequestEvent(
     val group: Group, val qq: UserInfo, message: String
-) : NewRequestEvent(group.yuq, message),
+) : NewRequestEvent(group.bot, message),
     CancelEvent {
     override fun cancelAble() = true
     val blackList = false
 }
 
-open class GroupMemberEvent(val group: Group, val member: Member) : Event(), BotEvent {
+open class GroupMemberEvent(val group: Group, val member: GroupMember) : Event(), BotEvent {
     override val bot: Bot
-        get() = group.yuq
+        get() = group.bot
 }
 
-open class GroupMemberJoinEvent(group: Group, member: Member) : GroupMemberEvent(group, member) {
-    open class Join(group: Group, member: Member) : GroupMemberJoinEvent(group, member)
-    open class Invite(group: Group, member: Member, val inviter: Member) : GroupMemberJoinEvent(group, member)
+open class GroupMemberJoinEvent(group: Group, member: GroupMember) : GroupMemberEvent(group, member) {
+    open class Join(group: Group, member: GroupMember) : GroupMemberJoinEvent(group, member)
+    open class Invite(group: Group, member: GroupMember, val inviter: GroupMember) : GroupMemberJoinEvent(group, member)
 }
 
 @Deprecated("群事件结构调整，使得命名语义更加清晰。", ReplaceWith("GroupMemberJoinEvent.Invite"))
-open class GroupMemberInviteEvent(group: Group, member: Member, inviter: Member) :
+open class GroupMemberInviteEvent(group: Group, member: GroupMember, inviter: GroupMember) :
     GroupMemberJoinEvent.Invite(group, member, inviter)
 
 
-open class GroupMemberLeaveEvent(group: Group, member: Member) : GroupMemberEvent(group, member) {
-    open class Leave(group: Group, member: Member) : GroupMemberLeaveEvent(group, member)
-    open class Kick(group: Group, member: Member, val operator: Member) : GroupMemberLeaveEvent(group, member)
+open class GroupMemberLeaveEvent(group: Group, member: GroupMember) : GroupMemberEvent(group, member) {
+    open class Leave(group: Group, member: GroupMember) : GroupMemberLeaveEvent(group, member)
+    open class Kick(group: Group, member: GroupMember, val operator: GroupMember) : GroupMemberLeaveEvent(group, member)
 }
 
 @Deprecated("群事件结构调整，使得命名语义更加清晰。", ReplaceWith("GroupMemberLeaveEvent.Kick"))
-open class GroupMemberKickEvent(group: Group, member: Member, operator: Member) :
+open class GroupMemberKickEvent(group: Group, member: GroupMember, operator: GroupMember) :
     GroupMemberLeaveEvent.Kick(group, member, operator)
 
-open class GroupBanMemberEvent(group: Group, member: Member, val operator: Member, val time: Int) :
+open class GroupBanMemberEvent(group: Group, member: GroupMember, val operator: GroupMember, val time: Int) :
     GroupMemberEvent(group, member)
 
-open class GroupUnBanMemberEvent(group: Group, member: Member, val operator: Member) : GroupMemberEvent(group, member)
-open class GroupBanBotEvent(group: Group, member: Member, val operator: Member, val time: Int) :
+open class GroupUnBanMemberEvent(group: Group, member: GroupMember, val operator: GroupMember) : GroupMemberEvent(group, member)
+open class GroupBanBotEvent(group: Group, member: GroupMember, val operator: GroupMember, val time: Int) :
     GroupMemberEvent(group, member)
 
-open class GroupUnBanBotEvent(group: Group, member: Member, val operator: Member) : GroupMemberEvent(group, member)
+open class GroupUnBanBotEvent(group: Group, member: GroupMember, val operator: GroupMember) : GroupMemberEvent(group, member)
 
 open class ContextSessionCreateEvent(override val bot: Bot, session: ContactSession) : Event(), BotEvent
 open class ActionContextInvokeEvent(val context: BotActionContext) : Event(), CancelEvent {
@@ -153,7 +153,7 @@ open class SendMessageEvent(val sendTo: Contact, val message: Message) : Event()
         SendMessageEvent(sendTo, message)
 
     override val bot: Bot
-        get() = sendTo.yuq
+        get() = sendTo.bot
 }
 
 // 消息发送未达预期事件。
@@ -173,13 +173,13 @@ open class SendMessageInvalidEvent(
     class ByReadTimeout(sendTo: Contact, message: Message) : SendMessageInvalidEvent(sendTo, message)
 
     override val bot: Bot
-        get() = sendTo.yuq
+        get() = sendTo.bot
 }
 
 
 open class ClickEvent(open val operator: Contact, val action: String, val suffix: String) : Event(), BotEvent {
     override val bot: Bot
-        get() = operator.yuq
+        get() = operator.bot
 }
 
 open class ClickBotEvent(operator: Contact, action: String, suffix: String) : ClickEvent(operator, action, suffix) {
@@ -187,16 +187,16 @@ open class ClickBotEvent(operator: Contact, action: String, suffix: String) : Cl
         open class FriendClick(override val operator: Friend, action: String, suffix: String) :
             Private(operator, action, suffix)
 
-        open class TempClick(override val operator: Member, action: String, suffix: String) :
+        open class TempClick(override val operator: GroupMember, action: String, suffix: String) :
             Private(operator, action, suffix)
     }
 
-    open class Group(override val operator: Member, action: String, suffix: String) :
+    open class Group(override val operator: GroupMember, action: String, suffix: String) :
         ClickBotEvent(operator, action, suffix)
 }
 
 open class AtBotEvent(open val type: Int, open val sender: Contact, open val source: Contact) : Event(), BotEvent {
-    open class ByGroup(type: Int, override val sender: Member, override val source: Group) :
+    open class ByGroup(type: Int, override val sender: GroupMember, override val source: Group) :
         AtBotEvent(type, sender, source)
 
     open class ByGuild(type: Int, override val sender: GuildMember, override val source: Channel, val guild: Guild) :
@@ -206,12 +206,12 @@ open class AtBotEvent(open val type: Int, open val sender: Contact, open val sou
         open class ByFriend(type: Int, override val sender: Friend, override val source: Friend) :
             ByPrivate(type, sender, source)
 
-        open class ByTemp(type: Int, override val sender: Member, override val source: Member) :
+        open class ByTemp(type: Int, override val sender: GroupMember, override val source: GroupMember) :
             ByPrivate(type, sender, source)
     }
 
     override val bot: Bot
-        get() = sender.yuq
+        get() = sender.bot
 }
 
 open class ClickSomeBodyEvent(operator: Contact, open val target: Contact, action: String, suffix: String) :
@@ -219,6 +219,6 @@ open class ClickSomeBodyEvent(operator: Contact, open val target: Contact, actio
     open class Private(operator: Contact, target: Contact, action: String, suffix: String) :
         ClickSomeBodyEvent(operator, target, action, suffix)
 
-    open class Group(override val operator: Member, override val target: Member, action: String, suffix: String) :
+    open class Group(override val operator: GroupMember, override val target: GroupMember, action: String, suffix: String) :
         ClickSomeBodyEvent(operator, target, action, suffix)
 }
